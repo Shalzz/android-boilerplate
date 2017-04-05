00b272daf253f2e9ae39555da6303a081449c729
Shaleen Jain <shaleen.jain95@gmail.com>
2017-04-05 11:41:59 +0530

    add Bugsnag Tree

--- app/src/main/java/in/eightbitlabs/androidboilerplate/BoilerplateApplication.java
+++ app/src/main/java/in/eightbitlabs/androidboilerplate/BoilerplateApplication.java
@@ -3,9 +3,12 @@ package in.eightbitlabs.androidboilerplate;
 import android.app.Application;
 import android.content.Context;
 
+import com.bugsnag.android.Bugsnag;
+
 import in.eightbitlabs.androidboilerplate.injection.component.ApplicationComponent;
 import in.eightbitlabs.androidboilerplate.injection.component.DaggerApplicationComponent;
 import in.eightbitlabs.androidboilerplate.injection.module.ApplicationModule;
+import in.eightbitlabs.androidboilerplate.util.BugsnagTree;
 import timber.log.Timber;
 
 public class BoilerplateApplication extends Application  {
@@ -15,6 +18,10 @@ public class BoilerplateApplication extends Application  {
     @Override
     public void onCreate() {
         super.onCreate();
+
+          // TODO: uncomment this when you have added the Bugsnag API key
+//        Bugsnag.init(this);
+//        Timber.plant(new BugsnagTree());
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
